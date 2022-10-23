781f7383cb18f0d58e892ed782a8a6cc0c31b29c
Sardorbek <jf.sardorbek@gmail.com>
2022-10-23 21:52:29 +0500

    this is my second attempt

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -4,11 +4,7 @@ public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
         variable = 2000;
-        int value = 1000;
         variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        value = 90000;
+        int value = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
         int variableOne = 1;
         int variableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
-        int variableSix = 6;
-        int variableSeven = 7;
-        int variableEight = 8;
     }
 }
